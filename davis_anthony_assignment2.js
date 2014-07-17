2d57e9fbe95acc7e6be7758c4c5bd22a0584322b
anthonydavis33 <anthony.davis33@gmail.com>
2014-07-17 10:28:48 -0400

    Small updates
    actually putting work into the code instead of pseudocoding all the
    time. -_-

--- davis_anthony_assignment2.js
+++ davis_anthony_assignment2.js
@@ -1,1 +1,25 @@
-alert("JavaScript works!");
+/* Anthony Davis
+* 07/17/2014
+* Project 2
+* Dragon Owner Acceptance
+*/
+
+// Initial Variables
+var     yourName,
+        dragonsTrained,
+        hasTrained
+;
+
+// Prompt
+
+// Procedure
+
+// Confirm
+
+// Boolean Function
+
+// Number Function
+
+// String Function
+
+// Output
